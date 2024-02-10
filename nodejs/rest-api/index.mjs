d9224ba5389a111d10b3997d34e009a0a58e0fda
Janaka Sandaruwan <35717600+JanakaSandaruwan@users.noreply.github.com>
2024-02-10 21:43:47 +0530

    Update index.mjs

--- nodejs/rest-api/index.mjs
+++ nodejs/rest-api/index.mjs
@@ -3,5 +3,5 @@ import app from "./app.mjs";
 const PORT = parseInt(process.env.PORT) || 8080;
 
 app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
